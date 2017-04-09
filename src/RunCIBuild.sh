3ca330cb12201155aa03b6e693dc2b19b5f18f36
Karel Golberg <karel.golberg@gmail.com>
2017-04-09 22:24:55 +0300

    CI fixes

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/bash -c "dotnet restore ./Ubora.sln && sh RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish"
-docker-compose -f "docker-compose.ci.build.yml" run ci-build
+exit $?
--- src/RunDockerizedCIBuild.sh
+++ src/RunDockerizedCIBuild.sh
@@ -1,2 +1,4 @@
 docker-compose -f "docker-compose.ci.build.yml" run ci-build
+buildresult=$?
 docker-compose -f "docker-compose.ci.build.yml" down
+exit=$buildresult
