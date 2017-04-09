0b0a37c0c244e7d1a75e14deb24edb3092824ecc
Karel Golberg <karel.golberg@gmail.com>
2017-04-09 22:50:36 +0300

    CI fixes

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-/bin/bash -c "dotnet restore ./Ubora.sln && sh RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish"
-exit $?
+dotnet restore ./Ubora.sln && sh RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
