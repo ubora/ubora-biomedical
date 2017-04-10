00b9d62de07cf59361a02e423828e0de15eeb66b
Karel Golberg <karel.golberg@gmail.com>
2017-04-10 18:45:55 +0300

    CI fix

--- src/RunDockerizedCIBuild.sh
+++ src/RunDockerizedCIBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-
 chmod +x RunCIBuild.sh
 docker-compose -f "docker-compose.ci.build.yml" run ci-build
+exitCode=$?
 docker-compose -f "docker-compose.ci.build.yml" down
+exit $exitCode
