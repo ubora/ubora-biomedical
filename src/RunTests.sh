15f7d42146e380881dbc9dbf21a2561881e40b73
Karel Golberg <karel.golberg@gmail.com>
2017-04-14 19:16:12 +0300

    Build script updated with new test project. VSCode tasks updated.

--- src/RunTests.sh
+++ src/RunTests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-set -e
-
-cd Ubora.Domain.Tests && dotnet xunit
+# Runs all tests and returns nonzero exit code, if any failed
+(cd Ubora.Domain.Tests && dotnet xunit)
+exit_code=$?
+(cd Ubora.Web.Tests && dotnet xunit)
+let exit_code=$?+$exit_code
+if [ $exit_code -ne 0 ]; then
+    echo "Some tests failed!"
+fi
+exit $exit_code
