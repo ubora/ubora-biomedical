58171c747cabd43cf57cbbac3f2629f74de61a4d
kasparkallas <kaspar.kallas@agileworks.eu>
2017-04-27 14:58:33 +0300

    Add JetBrains Annotations for navigating to FeatureFolder views

---
+++ src/Ubora.Web/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+﻿using JetBrains.Annotations;
+
+// https://youtrack.jetbrains.com/issue/RSRP-461882
+[assembly: AspMvcViewLocationFormat(@"~\Features\{1}\{0}.cshtml")]
+[assembly: AspMvcViewLocationFormat(@"~\Features\Shared\{0}.cshtml")]
