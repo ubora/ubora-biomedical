c1d5edb06d6c89ae06433667063bb6ba7ec5aa4f
kasparkallas <kaspar.kallas@agileworks.eu>
2017-05-11 16:21:27 +0300

    Move Account and Manage under Users

--- src/Ubora.Web/AssemblyAttributes.cs
+++ src/Ubora.Web/AssemblyAttributes.cs
@@ -1,9 +1,14 @@
 ﻿using JetBrains.Annotations;
 
 // https://youtrack.jetbrains.com/issue/RSRP-461882
-[assembly: AspMvcViewLocationFormat(@"~\_Features\Projects\{1}\{0}.cshtml")]
-[assembly: AspMvcViewLocationFormat(@"~\_Features\_Shared\{0}.cshtml")]
 
-[assembly: AspMvcViewLocationFormat(@"~\Features\ProjectManagement\Tasks\{0}.cshtml")]
-[assembly: AspMvcViewLocationFormat(@"~\Features\Shared\{0}.cshtml")]
-[assembly: AspMvcPartialViewLocationFormat(@"~\Features\Shared\")]
+// {0} - Action Name
+// {1} - Controller Name
+// {2} - Area Name
+// {3} - Feature Name
+
+[assembly: AspMvcViewLocationFormat(@"~\_Features\_Shared\{0}.cshtml")]
+[assembly: AspMvcPartialViewLocationFormat(@"~\_Features\_Shared\")]
+
+[assembly: AspMvcViewLocationFormat(@"~\_Features\Users\{1}\{0}.cshtml")]
+[assembly: AspMvcViewLocationFormat(@"~\_Features\Projects\{1}\{0}.cshtml")]
