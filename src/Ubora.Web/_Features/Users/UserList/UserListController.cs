dce7e19db1c605dd71eb1ee542c2fee30da0b338
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-05-26 13:02:57 +0300

    Replace ExecuteQuery to Find.

--- src/Ubora.Web/_Features/Users/UserList/UserListController.cs
+++ src/Ubora.Web/_Features/Users/UserList/UserListController.cs
@@ -1,11 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Ubora.Domain.Infrastructure;
-using Ubora.Domain.Infrastructure.Queries;
 using Ubora.Domain.Users;
-using Ubora.Web.Infrastructure;
 
 namespace Ubora.Web._Features.Users.UserList
 {
@@ -20,28 +17,10 @@ namespace Ubora.Web._Features.Users.UserList
 
         public IActionResult Index()
         {
-            List<UserProfile> userProfiles = ExecuteQuery(new GetUserProfileListQuery()).ToList();
-
+            var userProfiles = Find<UserProfile>();
             var userListItemViewModels = _mapper.Map(userProfiles, new List<UserListItemViewModel>());
 
             return View(userListItemViewModels);
-        }
-
-        public class GetUserProfileListQuery : IQuery<IEnumerable<UserProfile>>
-        {
-            public class Handler : ViewModelQueryHandler<GetUserProfileListQuery, IEnumerable<UserProfile>>
-            {
-                public Handler(IQueryProcessor queryProcessor) : base(queryProcessor)
-                {
-                }
-
-                public override IEnumerable<UserProfile> Handle(GetUserProfileListQuery query)
-                {
-                    var users = QueryProcessor.Find<UserProfile>();
-                        
-                    return users;
-                }
-            }
         }
     }
 }
