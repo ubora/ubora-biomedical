5666acb354f2d13903c6b65552caea3371614f31
kaarel_rebase <kaarel.rebase@gmail.com>
2017-06-19 17:06:19 +0300

    Fix

--- src/Ubora.Web.FunctionalTests/features/step-definitions/given.js
+++ src/Ubora.Web.FunctionalTests/features/step-definitions/given.js
@@ -2,8 +2,7 @@ const expect = require('chai').expect;
 
 module.exports = function () {
     this.Given(/^I go to the website "([^"]*)"$/, (url) => {
-        const newUrl = url;
-        browser.url(newUrl);
+        browser.url(url);
     });
     
 }
--- src/Ubora.Web.FunctionalTests/features/step-definitions/then.js
+++ src/Ubora.Web.FunctionalTests/features/step-definitions/then.js
@@ -2,9 +2,7 @@ const expect = require('chai').expect;
 
 module.exports = function () {
     this.Then(/^I expect the title of the page "([^"]*)"$/, (title) => {
-        const browserTwo = browser.getTitle();
-        console.error(browserTwo);
-        expect(browserTwo).to.be.eql(title);
+        expect(browser.getTitle()).to.be.eql(title);
     });
 
     this.Then(/^I expect that the element "([^"]*)?" is visible$/, (element) => {
