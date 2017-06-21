27c230b5d544b6d1eb4f6d0b539d9615581601a1
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-06-21 15:50:14 +0300

    Refactor TitleContains

--- src/Ubora.Domain/Projects/Project.TitleContains.cs
+++ src/Ubora.Domain/Projects/Project.TitleContains.cs
@@ -1,12 +1,11 @@
 ﻿using Marten.Util;
 using System;
-using System.Linq;
 using System.Linq.Expressions;
 using Ubora.Domain.Infrastructure.Specifications;
 
 namespace Ubora.Domain.Projects
 {
-    public class TitleContains<T> : Specification<Project> where T : IQueryable<Project>
+    public class TitleContains : Specification<Project>
     {
         public string Title { get; }
 
@@ -18,14 +17,7 @@ namespace Ubora.Domain.Projects
         //Method string.ToLower() doesn't works well in Query from marten
         internal override Expression<Func<Project, bool>> ToExpression()
         {
-            return p => p.Title.Contains(Title.ToLower(), StringComparison.CurrentCultureIgnoreCase);
-        }
-    }
-
-    public class TitleContains : TitleContains<IQueryable<Project>>
-    {
-        public TitleContains(string title) : base(title)
-        {
+            return p => p.Title.Contains(Title, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
