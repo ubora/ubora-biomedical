d4ed2438e774dea9f0b52f5b4d02282fe321a2d6
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-06-21 14:43:28 +0300

    Add profile pictures on the members page

--- src/Ubora.Web/_Features/Users/Profile/UserProfileViewModel.cs
+++ src/Ubora.Web/_Features/Users/Profile/UserProfileViewModel.cs
@@ -10,6 +10,5 @@
         public string Biography { get; set; }
         public string Skills { get; set; }
         public string Role { get; set; }
-        public string ProfilePictureLink { get; set; }
     }
 }
