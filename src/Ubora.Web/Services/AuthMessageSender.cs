8a221e79cebab71c97744eb5d4f7816b461a3edb
Risto Kitsing <risto.kitsing@agileworks.eu>
2017-06-28 10:33:42 +0300

    Added minimalistic e-mail template (needs finishing); refined "reset password" views

--- src/Ubora.Web/Services/AuthMessageSender.cs
+++ src/Ubora.Web/Services/AuthMessageSender.cs
@@ -29,12 +29,12 @@ namespace Ubora.Web.Services
 
         public async void SendForgotPasswordMessage(ApplicationUser user)
         {
-            var subject = "Reset Password";
+            var subject = "Password reset";
             var code = await _userManager.GeneratePasswordResetTokenAsync(user);
             var callbackUrl = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext)
                 .Action("ResetPassword", "Account", new { userId = user.Id, code },
                     _actionContextAccessor.ActionContext.HttpContext.Request.Scheme);
-            var message = "Please reset your password by clicking here: <a href=\"" + callbackUrl + "\">link</a>";
+            var message = "<h1 style='color:#4777BB;'>Password reset</h1><p>You can reset your password by clicking <a href=\"" + callbackUrl + "\">this link</a>.</p>";
             await _emailSender.SendEmailAsync(user.Email, subject, message);
         }
     }
