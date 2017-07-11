6564e0652fb1cf85d749b63ee6650ddc6075647b
kasparkallas <kaspar.kallas@agileworks.eu>
2017-07-11 16:07:21 +0300

    Bring back PrototypeController

---
+++ src/Ubora.Web/_Features/Prototype/PrototypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ubora.Web._Features.Prototype
+{
+    public class PrototypeController : UboraController
+    {
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
