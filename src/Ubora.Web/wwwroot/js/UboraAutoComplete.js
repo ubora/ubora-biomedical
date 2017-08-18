84d8ad106f111ad887ecfe78112bce4d973ae859
Liisa Suurkaev <liisa.suurkaev@agileworks.eu>
2017-08-18 11:02:22 +0300

    Moved AutoComplete script from Ubora.js to UboraAutoComplete.js

---
+++ src/Ubora.Web/wwwroot/js/UboraAutoComplete.js
@@ -0,0 +1,31 @@
+﻿function autoComplete(searchUsersUrl) {
+    AutoComplete({
+        Limit: 100,
+        MinChars: 2,
+        Url: searchUsersUrl,
+        QueryArg: "searchPhrase",
+        EmptyMessage: "No results",
+        _RenderResponseItems: function (response) {
+            var ul = document.createElement("ul"),
+                li = document.createElement("li"),
+                limit = this._Limit();
+
+            // Order
+            if (limit < 0) {
+                response = response.reverse();
+            } else if (limit === 0) {
+                limit = response.length;
+            }
+
+            for (var item = 0; item < Math.min(Math.abs(limit), response.length); item++) {
+                li.innerHTML = response[item].Label + ' - ' + response[item].Value;
+                li.setAttribute("data-autocomplete-value", response[item].Value);
+
+                ul.appendChild(li);
+                li = document.createElement("li");
+            }
+
+            return ul;
+        },
+    }, "#autocomplete");
+}
