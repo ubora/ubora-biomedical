057bef891b89c5e1efcac46f9d3f6a2ec24c7250
kasparkallas <kaspar.kallas@agileworks.eu>
2017-08-18 14:58:49 +0300

    Add diagnostics tests framework with first test for notification view model partial views

---
+++ src/Ubora.Web/_Features/Admin/Tests/DiagnosticsTestResult.cs
@@ -0,0 +1,9 @@
+namespace Ubora.Web._Features.Admin.Tests
+{
+    public class DiagnosticsTestResult
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+    }
+}
