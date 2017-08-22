9da4a7baa033b112a25db096c5d1f25f94e501cb
Liisa Suurkaev <liisa.suurkaev@agileworks.eu>
2017-08-22 11:32:58 +0300

    Edited simpleMDE, removed quote from editor

--- src/Ubora.Web/wwwroot/js/UBORA.js
+++ src/Ubora.Web/wwwroot/js/UBORA.js
@@ -1,20 +1,20 @@
 // TODO: Add to ES6 module under 'sidemenu' namespace + Babel
 const toggleSideMenu = () => {
-  return document
-      .querySelector('.js-sidemenu')
-      .classList
+    return document
+        .querySelector('.js-sidemenu')
+        .classList
         .toggle('sidemenu-open');
 };
 
 document
-  .querySelectorAll('.js-sidemenu-toggle')
-  .forEach(element => {
-    element.addEventListener('click', toggleSideMenu);
-    element.addEventListener('keyup', event => {
-      const isEnterPressed = event.code === 'Enter';
-      isEnterPressed ? toggleSideMenu() : undefined;
+    .querySelectorAll('.js-sidemenu-toggle')
+    .forEach(element => {
+        element.addEventListener('click', toggleSideMenu);
+        element.addEventListener('keyup', event => {
+            const isEnterPressed = event.code === 'Enter';
+            isEnterPressed ? toggleSideMenu() : undefined;
+        });
     });
-});
 
 window.addEventListener('load', function () {
     // Target is to have this number in production setting < 500ms
@@ -58,7 +58,94 @@ if (markdownArrayHasItems) {
 var simpleMde = new SimpleMDE({
     element: document.querySelector('.content_editable'), previewRender: function (plainText) {
         return marked(plainText, { renderer: renderer });
+    },
+    toolbar: [{
+        name: "bold",
+        action: SimpleMDE.toggleBold,
+        className: "fa fa-bold",
+        title: "Bold",
+    },
+    {
+        name: "italic",
+        action: SimpleMDE.toggleItalic,
+        className: "fa fa-italic",
+        title: "Italic",
+        default: true
+    },
+    {
+        name: "heading",
+        action: SimpleMDE.toggleHeadingSmaller,
+        className: "fa fa-header",
+        title: "Heading",
+        default: true
+    },
+        "|",
+    {
+        name: "unordered-list",
+        action: SimpleMDE.toggleUnorderedList,
+        className: "fa fa-list-ul",
+        title: "Generic List",
+        default: true
+    },
+    {
+        name: "ordered-list",
+        action: SimpleMDE.toggleOrderedList,
+        className: "fa fa-list-ol",
+        title: "Numbered List",
+        default: true
+    },
+        "|",
+    {
+        name: "link",
+        action: SimpleMDE.drawLink,
+        className: "fa fa-link",
+        title: "Create Link",
+        default: true
+    },
+    {
+        name: "image",
+        action: SimpleMDE.drawImage,
+        className: "fa fa-picture-o",
+        title: "Insert Image",
+        default: true
+    },
+        "|",
+    {
+        name: "preview",
+        action: SimpleMDE.togglePreview,
+        className: "fa fa-eye no-disable",
+        title: "Toggle Preview",
+        default: true
+    },
+    {
+        name: "fullscreen",
+        action: SimpleMDE.toggleFullScreen,
+        className: "fa fa-arrows-alt no-disable no-mobile",
+        title: "Toggle Fullscreen",
+        default: true
+    },
+        "|",
+    {
+        name: "guide",
+        action: "https://simplemde.com/markdown-guide",
+        className: "fa fa-question-circle",
+        title: "Markdown Guide",
+        default: true
+    },
+        "|",
+    {
+        name: "undo",
+        action: SimpleMDE.undo,
+        className: "fa fa-undo no-disable",
+        title: "Undo"
+    },
+    {
+        name: "redo",
+        action: SimpleMDE.redo,
+        className: "fa fa-repeat no-disable",
+        title: "Redo"
     }
+    ],
 });
 
 // Feedback modal
@@ -119,23 +206,23 @@ function feedbackModule(requestPath, sendFeedbackUrl) {
 
 // Notices
 function dismissNotice(userEvent) {
-  function removeParentElement(currentElement) {
-    var parentElement = currentElement.parentElement;
-    parentElement.remove();
-  }
+    function removeParentElement(currentElement) {
+        var parentElement = currentElement.parentElement;
+        parentElement.remove();
+    }
 
-  var currentElement = userEvent.target;
-  var notificationElement = currentElement.classList.contains('js-notice-close');
+    var currentElement = userEvent.target;
+    var notificationElement = currentElement.classList.contains('js-notice-close');
 
-  if (notificationElement) {
-    removeParentElement(currentElement);
-  }
+    if (notificationElement) {
+        removeParentElement(currentElement);
+    }
 }
 
 window.addEventListener('click', function (event) {
-  dismissNotice(event);
+    dismissNotice(event);
 });
 
 window.addEventListener('touchend', function (event) {
-  dismissNotice(event);
+    dismissNotice(event);
 }, false);
