d20a9db4c1eb2f7cd160f313a9ab7c8ca4cc0d68
kasparkallas <kaspar.kallas@agileworks.eu>
2017-08-28 14:26:25 +0300

    Fix missing using

--- src/Ubora.Domain.Tests/Users/Specifications/UserEmailContainsPhraseSpecTests.cs
+++ src/Ubora.Domain.Tests/Users/Specifications/UserEmailContainsPhraseSpecTests.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
 using System;
-using Ubora.Domain.Tests.Helper;
 using Ubora.Domain.Users;
 using Ubora.Domain.Users.Specifications;
 using Xunit;
@@ -13,7 +12,7 @@ namespace Ubora.Domain.Tests.Users.Specifications
         public void Returns_True_When_User_Email_Consists_Search_Phrase()
         {
             var userProfile = new UserProfile(Guid.NewGuid());
-            userProfile.SetPropertyValue(nameof(UserProfile.Email), "tEst@agileworks.eu");
+            userProfile.Set(nameof(UserProfile.Email), "tEst@agileworks.eu");
 
             var searchPhrase = "es";
             var specification = new UserEmailContainsPhraseSpec(searchPhrase);
@@ -29,7 +28,7 @@ namespace Ubora.Domain.Tests.Users.Specifications
         public void Returns_False_When_User_Email_Does_Not_Consist_Search_Phrase()
         {
             var userProfile = new UserProfile(Guid.NewGuid());
-            userProfile.SetPropertyValue(nameof(UserProfile.Email), "test@agileworks.eu");
+            userProfile.Set(nameof(UserProfile.Email), "test@agileworks.eu");
 
             var searchPhrase = "as";
             var specification = new UserEmailContainsPhraseSpec(searchPhrase);
--- src/Ubora.Domain.Tests/Users/Specifications/UserFullNameContainsPhraseSpecTests.cs
+++ src/Ubora.Domain.Tests/Users/Specifications/UserFullNameContainsPhraseSpecTests.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
 using System;
-using Ubora.Domain.Tests.Helper;
 using Ubora.Domain.Users;
 using Ubora.Domain.Users.Specifications;
 using Xunit;
@@ -13,8 +12,8 @@ namespace Ubora.Domain.Tests.Users.Specifications
         public void Returns_True_When_User_FullName_Consists_Search_Phrase()
         {
             var userProfile = new UserProfile(Guid.NewGuid());
-            userProfile.SetPropertyValue(nameof(UserProfile.FirstName), "FirstName");
-            userProfile.SetPropertyValue(nameof(UserProfile.LastName), "LastName");
+            userProfile.Set(nameof(UserProfile.FirstName), "FirstName");
+            userProfile.Set(nameof(UserProfile.LastName), "LastName");
 
             var searchPhrase = "na";
             var specification = new UserFullNameContainsPhraseSpec(searchPhrase);
@@ -30,8 +29,8 @@ namespace Ubora.Domain.Tests.Users.Specifications
         public void Returns_False_When_User_FullName_Does_Not_Consist_Search_Phrase()
         {
             var userProfile = new UserProfile(Guid.NewGuid());
-            userProfile.SetPropertyValue(nameof(UserProfile.LastName), "LastName");
-            userProfile.SetPropertyValue(nameof(UserProfile.FirstName), "FirstName");
+            userProfile.Set(nameof(UserProfile.LastName), "LastName");
+            userProfile.Set(nameof(UserProfile.FirstName), "FirstName");
 
             var searchPhrase = "no";
             var specification = new UserFullNameContainsPhraseSpec(searchPhrase);
