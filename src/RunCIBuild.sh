637bd4c21d43ee11c4fb8326f4e4332beb3eeb43
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-08-31 17:44:55 +0300

    Add script and clean up.

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
+rm -f -r Ubora.Web/obj/Docker/publish
 dotnet restore ./Ubora.sln && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
---
+++ src/RunDockerizedCIFunctionaltests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod +x Ubora.Web.FunctionalTests/wait-for-it.sh
+docker-compose -f "docker-compose.ci.functional-tests.yml" run -e FunctionalTest functional.tests
+exitCode=$?
+docker-compose -f "docker-compose.ci.functional-tests.yml" down
+exit $exitCode
