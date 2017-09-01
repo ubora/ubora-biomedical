752d3b9690df8f224fb1abb310dbdc3571d7c18a
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-09-01 12:02:04 +0300

    Add docker compose functional tests build.

--- src/RunDockerizedCIFunctionaltests.sh
+++ src/RunDockerizedCIFunctionaltests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod +x Ubora.Web.FunctionalTests/wait-for-it.sh
+docker-compose -f "docker-compose.ci.functional-tests.yml" build
 docker-compose -f "docker-compose.ci.functional-tests.yml" run -e FunctionalTest functional.tests
 exitCode=$?
 docker-compose -f "docker-compose.ci.functional-tests.yml" down
