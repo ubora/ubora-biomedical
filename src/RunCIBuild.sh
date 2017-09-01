18015be7505af2a18354065e1d2f441ce6ce72ed
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-09-01 11:20:46 +0300

    Add clean and bower install in script.

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+dotnet clean ./Ubora.sln --configuration Release
 rm -f -r Ubora.Web/obj/Docker/publish
-dotnet restore ./Ubora.sln && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
+dotnet restore ./Ubora.sln && bash RunTests.sh && (cd Ubora.Web && bower install --force) && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
