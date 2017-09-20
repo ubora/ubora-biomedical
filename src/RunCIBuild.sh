f10bc8575f47a7b5b920c7033fdb2828b1872b88
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-09-20 17:50:10 +0300

    Add npm install.

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -3,4 +3,4 @@ set -e
 
 dotnet clean ./Ubora.sln --configuration Release
 rm -f -r Ubora.Web/obj/Docker/publish
-dotnet restore ./Ubora.sln && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
+dotnet restore ./Ubora.sln && (cd Ubora.Web && npm install --force) && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
