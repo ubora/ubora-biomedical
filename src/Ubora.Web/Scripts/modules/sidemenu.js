031c6d714ffca2cd484666b1f574570061d0c9b7
Risto Kitsing <risto.kitsing@agileworks.eu>
2017-09-27 14:54:46 +0300

    Refactored code to avoid .forEach() method in bad browsers

--- src/Ubora.Web/Scripts/modules/sidemenu.js
+++ src/Ubora.Web/Scripts/modules/sidemenu.js
@@ -1,14 +1,16 @@
 export class Sidemenu {
   constructor() {
-    document
-        .querySelectorAll('.js-sidemenu-toggle')
-        .forEach(element => {
-            element.addEventListener('click', this._toggleSidemenu);
-            element.addEventListener('keyup', event => {
-                const isEnterPressed = event.code === 'Enter';
-                isEnterPressed ? this._toggleSidemenu() : undefined;
-            });
-        });
+    const sidemenuToggleElementList = document.querySelectorAll('.js-sidemenu-toggle');
+
+    for (let i = 0; i < sidemenuToggleElementList.length; i++) {
+      let element = sidemenuToggleElementList[i];
+
+      element.addEventListener('click', this._toggleSidemenu);
+      element.addEventListener('keyup', event => {
+        const isEnterPressed = event.code === 'Enter';
+        isEnterPressed ? this._toggleSidemenu() : undefined;
+      });
+    }
   }
 
   _toggleSidemenu() {
