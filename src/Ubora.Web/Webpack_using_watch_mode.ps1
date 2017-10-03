34e7a699d8b746a7c34a8893a99992eedaf4efcd
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-10-03 12:38:52 +0300

    Remove watch mode in build.

--- src/Ubora.Web/Webpack_using_watch_mode.ps1
+++ src/Ubora.Web/Webpack_using_watch_mode.ps1
@@ -2,4 +2,4 @@ Write-Host "Installing node module dependencies:"
 npm install
 
 Write-Host "Bundling and watching with Webpack:"
-node ./node_modules/webpack/bin/webpack.js --watch --config webpack.config.js
+node ./node_modules/webpack/bin/webpack.js --watch --watch-poll --config webpack.config.js
