1d906636baa5217469143a35828eb5cb5f174394
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2017-10-06 15:25:08 +0300

    Test in build.

--- src/RunDockerizedCIFunctionaltests.sh
+++ src/RunDockerizedCIFunctionaltests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x Ubora.Web.FunctionalTests/wait-for-it.sh
 docker-compose -f "docker-compose.ci.functional-tests.yml" build
-docker-compose -f "docker-compose.ci.functional-tests.yml" run -T -e FunctionalTest functional.tests
+docker-compose -f "docker-compose.ci.functional-tests.yml" up --exit-code-from functional.tests
 exitCode=$?
 docker-compose -f "docker-compose.ci.functional-tests.yml" down
 exit $exitCode
