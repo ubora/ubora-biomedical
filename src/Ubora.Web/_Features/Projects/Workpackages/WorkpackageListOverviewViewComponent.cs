d735818c7bfc13b78acd4f86c234c136b1d68fef
Liisa Suurkaev <liisa.suurkaev@agileworks.eu>
2017-11-06 11:37:15 +0200

    Added working menuoptions

--- src/Ubora.Web/_Features/Projects/Workpackages/WorkpackageListOverviewViewComponent.cs
+++ src/Ubora.Web/_Features/Projects/Workpackages/WorkpackageListOverviewViewComponent.cs
@@ -3,10 +3,55 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Ubora.Domain.Infrastructure.Queries;
 using Ubora.Domain.Projects.Workpackages;
-using Ubora.Web._Features.Projects.Workpackages.Steps;
+using System.Collections.Generic;
 
 namespace Ubora.Web._Features.Projects.Workpackages
 {
+    public class WorkpackageMenuViewModel
+    {
+        public string WorkPackageId { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<MenuLink> MenuLinks { get; set; }
+        public bool HasBeenAccepted { get; set; }
+        public bool IsMuted { get; set; }
+
+        public string IconOpenSrc { get; set; }
+        public string IconMutedSrc { get; set; }
+
+        public string WorkPackageImageSrc
+        {
+            get
+            {
+                if (IsMuted)
+                {
+                    return IconMutedSrc;
+                }
+
+                if (HasBeenAccepted)
+                {
+                    return "/images/icons/check.svg";
+                }
+
+                return IconOpenSrc;
+            }
+        }
+
+        public bool IsAnyMenuLinkSelected
+        {
+            get
+            {
+                return MenuLinks.Any(x => x.IsSelected);
+            }
+        }
+    }
+
+    public class MenuLink
+    {
+        public bool IsSelected { get; set; }
+        public string Href { get; set; }
+        public string Name { get; set; }
+    }
+
     public class WorkpackageListOverviewViewComponent : ProjectViewComponent
     {
         public WorkpackageListOverviewViewComponent(IQueryProcessor processor)
@@ -18,42 +63,147 @@ namespace Ubora.Web._Features.Projects.Workpackages
         public async Task<IViewComponentResult> InvokeAsync()
 #pragma warning restore 1998
         {
-            var model = new WorkpackageListOverviewViewModel
-            {
-                WorkpackageOne = CreateViewModel<Domain.Projects.Workpackages.WorkpackageOne>(),
-                WorkpackageTwo = CreateViewModel<WorkpackageTwo>(),
-                WorkpackageThree = CreateViewModel<WorkpackageThree>()
-            };
+            var workPackageMenuViewModels = new List<WorkpackageMenuViewModel>();
+            workPackageMenuViewModels.Add(CreateWorkpackageOneMenuViewModel());
+            workPackageMenuViewModels.Add(CreateWorkpackageTwoMenuViewModel());
+            workPackageMenuViewModels.Add(CreateWorkpackageThreeMenuViewModel());
 
-            var selectedStepId = GetSelectedStepId();
-            if (selectedStepId != null)
-            {
-                model.MarkSelectedStep(selectedStepId);
-            }
-
-            return View("~/_Features/Projects/Workpackages/_WorkpackageListOverviewPartial.cshtml", model);
+            return View("~/_Features/Projects/Workpackages/_WorkpackageListOverviewPartial.cshtml", workPackageMenuViewModels);
         }
 
-        private WorkpackageOneOverviewViewModel CreateViewModel<T>() where T : Workpackage<T>
+        private WorkpackageMenuViewModel CreateWorkpackageOneMenuViewModel()
         {
-            var workpackage = QueryProcessor.FindById<T>(ProjectId);
+            var workpackage = QueryProcessor.FindById<WorkpackageOne>(ProjectId);
             if (workpackage == null)
             {
-                // TODO
-                return null;
+                return new WorkpackageMenuViewModel
+                {
+                    Title = "Medical need and product specification",
+                    WorkPackageId = "workPackageOne",
+                    MenuLinks = new List<MenuLink>(),
+                    IsMuted = true,
+                    IconMutedSrc = "/images/icons/one_muted.svg"
+                };
             }
 
-            var model = new WorkpackageOneOverviewViewModel
+            var menuLinks = workpackage.Steps.Select(step => new MenuLink
+            {
+                Name = step.Title,
+                IsSelected = GetSelectedStepId() == step.Id,
+                Href = Url.Action("Read", "WorkpackageOne", new { projectId = ProjectId, stepId = step.Id })
+            }).ToList();
+
+            menuLinks.Add(new MenuLink
+            {
+                Name = "Device classification",
+                Href = Url.Action("DeviceClassification", "WorkpackageOne"),
+                IsSelected = (this.Request.Path == Url.Action("DeviceClassification", "WorkpackageOne"))
+            });
+
+            menuLinks.Add(new MenuLink
+            {
+                Name = "Regulation checklist",
+                Href = Url.Action("Index", "ApplicableRegulations"),
+                IsSelected = (this.Request.Path == Url.Action("Index", "ApplicableRegulations"))
+            });
+
+            menuLinks.Add(new MenuLink
+            {
+                Name = "Mentor review",
+                Href = Url.Action("Review", "WorkpackageOneReview"),
+                IsSelected = (this.Request.Path == Url.Action("Review", "WorkpackageOneReview")
+                    || this.Request.Path == Url.Action("Decision", "WorkpackageOneReview"))
+            });
+
+            var workPackageMenu = new WorkpackageMenuViewModel
             {
                 Title = workpackage.Title,
-                Steps = workpackage.Steps.Select(task => new WorkpackageOneOverviewViewModel.Step
-                {
-                    Id = task.Id.ToString(),
-                    Title = task.Title
-                }).ToList()
+                WorkPackageId = "workpackageOne",
+                MenuLinks = menuLinks,
+                IconOpenSrc = "/images/icons/one_open.svg",
+                IconMutedSrc = "/images/icons/one_muted.svg",
+                HasBeenAccepted = workpackage.HasBeenAccepted
             };
 
-            return model;
+            return workPackageMenu;
+        }
+
+        private WorkpackageMenuViewModel CreateWorkpackageTwoMenuViewModel()
+        {
+            var workpackage = QueryProcessor.FindById<WorkpackageTwo>(ProjectId);
+            if (workpackage == null)
+            {
+                return new WorkpackageMenuViewModel
+                {
+                    Title = "Conceptual design",
+                    WorkPackageId = "workPackageTwo",
+                    MenuLinks = new List<MenuLink>(),
+                    IsMuted = true,
+                    IconMutedSrc = "/images/icons/two_muted.svg"
+                };
+            }
+
+            var menuLinks = workpackage.Steps.Select(step => new MenuLink
+            {
+                Name = step.Title,
+                IsSelected = GetSelectedStepId() == step.Id,
+                Href = Url.Action("Read", "WorkpackageTwo", new { projectId = ProjectId, stepId = step.Id })
+            }).ToList();
+
+            menuLinks.Add(new MenuLink
+            {
+                Name = "Mentor review",
+                Href = Url.Action("Review", "WorkpackageTwoReview"),
+                IsSelected = (this.Request.Path == Url.Action("Review", "WorkpackageTwoReview")
+                    || this.Request.Path == Url.Action("Decision", "WorkpackageTwoReview"))
+            });
+
+            var workPackageMenu = new WorkpackageMenuViewModel
+            {
+                Title = workpackage.Title,
+                WorkPackageId = "workpackageTwo",
+                MenuLinks = menuLinks,
+                IconOpenSrc = "/images/icons/two_open.svg",
+                IconMutedSrc = "/images/icons/two_muted.svg",
+                HasBeenAccepted = workpackage.HasBeenAccepted
+            };
+
+            return workPackageMenu;
+        }
+
+        private WorkpackageMenuViewModel CreateWorkpackageThreeMenuViewModel()
+        {
+            var workpackage = QueryProcessor.FindById<WorkpackageThree>(ProjectId);
+            if (workpackage == null)
+            {
+                return new WorkpackageMenuViewModel
+                {
+                    Title = "Design and prototyping",
+                    WorkPackageId = "workpackageThree",
+                    MenuLinks = new List<MenuLink>(),
+                    IsMuted = true,
+                    IconMutedSrc = "/images/icons/three_muted.svg"
+                };
+            }
+
+            var menuLinks = workpackage.Steps.Select(step => new MenuLink
+            {
+                Name = step.Title,
+                IsSelected = GetSelectedStepId() == step.Id,
+                Href = Url.Action("Read", "WorkpackageThree", new { projectId = ProjectId, stepId = step.Id })
+            }).ToList();
+
+            var workPackageMenu = new WorkpackageMenuViewModel
+            {
+                Title = workpackage.Title,
+                WorkPackageId = "workpackageThree",
+                MenuLinks = menuLinks,
+                IconOpenSrc = "/images/icons/three_open.svg",
+                IconMutedSrc = "/images/icons/three_muted.svg",
+                HasBeenAccepted = workpackage.HasBeenAccepted
+            };
+
+            return workPackageMenu;
         }
 
         private string GetSelectedStepId()
