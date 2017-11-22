f4f161bdecd36f4fd7466418b68896d93e5188d3
kasparkallas <kaspar.kallas@agileworks.eu>
2017-11-22 12:42:42 +0200

    Revert "Possible workaround fix for functional tests crashing in docker"
    This reverts commit 7ecd6c305edac4878ea61e121d10976467dcacb5.

--- src/RunDockerizedCIFunctionaltests.sh
+++ src/RunDockerizedCIFunctionaltests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 chmod +x Ubora.Web.FunctionalTests/wait-for-it.sh
-docker-compose -f "docker-compose.ci.functional-tests.yml" up --abort-on-container-exit --exit-code-from "functional.tests"
-#Check the docker-compose ps output and scan the exit codes and exit with an error code if any of the containers failed to exit gracefully
-docker-compose -f "docker-compose.ci.functional-tests.yml" ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do  
-    if [ "$code" != "0" ]; then    
-       exitCode=1
-    fi
-done 
+docker-compose -f "docker-compose.ci.functional-tests.yml" up --abort-on-container-exit --exit-code-from functional.tests
+exitCode=$?
 docker-compose -f "docker-compose.ci.functional-tests.yml" down
 exit $exitCode
