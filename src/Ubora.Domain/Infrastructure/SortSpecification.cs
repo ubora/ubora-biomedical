341a10616400a615f4623c94b52cdfc167c9c8f4
Karel Golberg <karel.golberg@agileworks.eu>
2017-12-01 13:33:38 +0200

    Removed unnecessary abstract SortSpecification class

--- src/Ubora.Domain/Infrastructure/SortSpecification.cs
+++ src/Ubora.Domain/Infrastructure/SortSpecification.cs
@@ -5,20 +5,11 @@ using System.Linq.Expressions;
 namespace Ubora.Domain.Infrastructure
 {
     /// <summary>
-    /// Do not use this interface directly for implementing specifications, use abstract SortSpecification<TEntity, Tkey> class for that.
+    /// NB! Only for sorting :)
     /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface ISortSpecification<TEntity>
     {
         IQueryable<TEntity> Sort(IQueryable<TEntity> query);
-    }
-
-    public abstract class SortSpecification<TEntity, TKey> : ISortSpecification<TEntity>
-    {
-        public IQueryable<TEntity> Sort(IQueryable<TEntity> query)
-        {
-            return Queryable.OrderBy(query, KeySelector);
-        }
-
-        internal abstract Expression<Func<TEntity, TKey>> KeySelector { get; }
     }
 }
