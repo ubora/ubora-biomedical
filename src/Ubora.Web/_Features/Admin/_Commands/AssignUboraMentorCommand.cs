b5392ab41d09667930de02b1dee504bb4d0c250e
kasparkallas <kaspar.kallas@agileworks.eu>
2017-12-01 18:39:00 +0200

    Command stubs

---
+++ src/Ubora.Web/_Features/Admin/_Commands/AssignUboraMentorCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ubora.Web._Features.Admin._Commands
+{
+    public class AppointUboraMentorCommand
+    {
+    }
+
+    public class DismissUboraMentorCommand
+    {
+    }
+}
