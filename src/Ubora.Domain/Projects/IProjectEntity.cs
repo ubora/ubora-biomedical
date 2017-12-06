3d2192895c4d7c554a6927e7e7c8a8e061b93b25
kasparkallas <kaspar.kallas@agileworks.eu>
2017-12-06 18:39:43 +0200

    Add ID back to IProjectEntity

--- src/Ubora.Domain/Projects/IProjectEntity.cs
+++ src/Ubora.Domain/Projects/IProjectEntity.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Ubora.Domain.Projects
 {
     public interface IProjectEntity
     {
+        Guid Id { get; }
         Guid ProjectId { get; }
     }
 }
