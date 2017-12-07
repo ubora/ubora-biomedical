393002b4e7f110a7a583fcbf7e622a231699c128
Liisa Suurkaev <liisa.suurkaev@agileworks.eu>
2017-12-07 17:30:46 +0200

    Added helpful tips

--- src/Ubora.Domain/Projects/Workpackages/WorkpackageThree.cs
+++ src/Ubora.Domain/Projects/Workpackages/WorkpackageThree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Resources;
 using Ubora.Domain.Projects.Workpackages.Events;
 
 namespace Ubora.Domain.Projects.Workpackages
@@ -17,23 +18,22 @@ namespace Ubora.Domain.Projects.Workpackages
 
             Title = "Design and prototyping";
 
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_CommercialParts", "Commercial parts", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_PurposelyDesignedParts", "Purposely designed parts", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_PrototypesAndFunctionalTrials", "Prototypes and functional trials", ""));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_CommercialParts", "Commercial parts"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_PurposelyDesignedParts", "Purposely designed parts"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Hardware_PrototypesAndFunctionalTrials", "Prototypes and functional trials"));
 
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_CommercialParts", "Commercial parts", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_PurposelyDesignedParts", "Purposely designed parts", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_PrototypesAndFunctionalTrials", "Prototypes and functional trials", ""));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_CommercialParts", "Commercial parts"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_PurposelyDesignedParts", "Purposely designed parts"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_ElectronicAndFirmware_PrototypesAndFunctionalTrials", "Prototypes and functional trials"));
 
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_ExistingSolutions", "Existing solutions (open source)", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_PurposelyDesignedParts", "Purposely designed parts", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_PrototypesAndFunctionalTrials", "Prototypes and functional trials", ""));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_ExistingSolutions", "Existing solutions (open source)"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_PurposelyDesignedParts", "Purposely designed parts"));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_Software_PrototypesAndFunctionalTrials", "Prototypes and functional trials"));
 
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_SystemIntegration_Optimization", "Optimization: materials, processes and performance", ""));
-            _steps.Add(new WorkpackageStep("GeneralProductDescription_SystemIntegration_PrototypesAndFunctionalTrials", "Prototypes and functional trials", ""));
+            _steps.Add(new WorkpackageStep("GeneralProductDescription_SystemIntegration_PrototypesAndFunctionalTrials", "Prototypes and functional trials"));
 
-            _steps.Add(new WorkpackageStep("DesignForIsoTestingCompliance", "Design for ISO testing compliance", ""));
-            _steps.Add(new WorkpackageStep("InstructionsForFabricationOfPrototypes", "Instructions for fabrication of prototypes", ""));
+            _steps.Add(new WorkpackageStep("DesignForIsoTestingCompliance", "Design for ISO testing compliance"));
+            _steps.Add(new WorkpackageStep("InstructionsForFabricationOfPrototypes", "Instructions for fabrication of prototypes"));
         }
 
         private void Apply(WorkpackageThreeStepEdited e)
