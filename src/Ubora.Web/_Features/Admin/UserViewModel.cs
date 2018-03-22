2a6db62e03fc7bae2928a7eba312f0a15fe8b7f5
Kärolin Kivisikk <karolin.kivisikk@gmail.com>
2018-03-22 16:29:50 +0200

    Admin view role fix

--- src/Ubora.Web/_Features/Admin/UserViewModel.cs
+++ src/Ubora.Web/_Features/Admin/UserViewModel.cs
@@ -13,7 +13,8 @@ namespace Ubora.Web._Features.Admin
         public string UserEmail { get; set; }
 
         public IEnumerable<string> Roles { get; set; }
-        public string RolesJoined => string.Join(", ", Roles); 
+        public IEnumerable<string> RolesWithoutPrefixes => Roles.Select(role => role.Replace("Ubora.", ""));
+        public string RolesJoined => string.Join(", ", RolesWithoutPrefixes); 
 
         public bool IsAdmin => Roles.Any(x => x == ApplicationRole.Admin);
         public bool IsMentor => Roles.Any(x => x == ApplicationRole.Mentor);
