2709b353ff172c0ef858b0a7ca7a4b780b033b76
DESKTOP-4NCNUF9\Praktika <martin.betlem@gmail.com>
2018-03-22 16:27:35 +0200

    Fix xunit and add explicitly version.

--- src/RunTests.sh
+++ src/RunTests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Runs all tests and returns nonzero exit code, if any failed
-(cd Ubora.Domain.Tests && dotnet xunit --configuration Release)
+# using -framework netcoreapp2.0 -fxversion 2.0.6, which aspnetcore-build targeting 2.0.6
+(cd Ubora.Domain.Tests && dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.6 --configuration Release)
 exit_code=$?
-(cd Ubora.Web.Tests && dotnet xunit --configuration Release)
+(cd Ubora.Web.Tests && dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.6 --configuration Release)
 let exit_code=$?+$exit_code
 if [ $exit_code -ne 0 ]; then
     echo "Some tests failed!"
