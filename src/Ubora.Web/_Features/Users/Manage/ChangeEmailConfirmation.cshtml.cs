93f069b275b05bb26fe527a8110864f1dd864b26
Kärolin Kivisikk <karolin.kivisikk@gmail.com>
2018-04-18 17:38:19 +0300

    Added confirmation, changed emails

---
+++ src/Ubora.Web/_Features/Users/Manage/ChangeEmailConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ubora.Web._Features.Users.Manage
+{
+    public class ChangeEmailConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
