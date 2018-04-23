68813408453d825291cff69cbd82b2e1f0e306c7
kasparkallas <kaspar.kallas@agileworks.eu>
2018-04-23 11:53:12 +0300

    Clean-up

--- src/Ubora.Web/_Features/Projects/Repository/FileViewModelBase.cs
+++ src/Ubora.Web/_Features/Projects/Repository/FileViewModelBase.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace Ubora.Web._Features.Projects.Repository
 {
     public abstract class FileViewModelBase
     {
+        private static readonly string[] Supported3DFileExtensions = new[] { ".stl", ".amf", ".nxz" };
+        
         public string Comment { get; set; }
         public string FileName { get; set; }
         public int RevisionNumber { get; set; }
 
         public bool Has3DFileExtension()
         {
-            var extension = System.IO.Path.GetExtension(FileName);
+            var extension = Path.GetExtension(FileName);
 
-            var supported3dFileExtensions = new[] { ".stl", ".amf", ".nxz" };
-            return supported3dFileExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
+            return Supported3DFileExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
         }
     }
 }
