b0b79e622b5559df7f6ac408f9336aafcaa4c5c0
kasparkallas <kaspar.kallas@agileworks.eu>
2018-05-30 19:27:57 +0300

    Combine infinite scroll and pagination

---
+++ src/Ubora.Web/Run Web.bat
@@ -0,0 +1,1 @@
+powershell -noexit -executionpolicy bypass -File "Run Web.ps1"
--- src/Ubora.Web/Scripts/modules/infinite_scroll.js
+++ src/Ubora.Web/Scripts/modules/infinite_scroll.js
@@ -1,22 +1,14 @@
 ﻿import InfiniteScroll from 'infinite-scroll'
 
-global.UBORA.initInfiniteScroll = function (path, pageCount, title) {
-    var infiniteScroll = new InfiniteScroll('.infinite-scroll', {
+global.UBORA.initInfiniteScroll = function (path) {
+    return new InfiniteScroll('.infinite-scroll', {
         path: function () {
-            if (this.pageIndex < pageCount && title !== undefined) {
-                var nextIndex = this.pageIndex + 1;
-                return '/ProjectList/Search/?title=' + title + path + nextIndex;
-            } else if (this.pageIndex < pageCount && title === undefined) {
-                var nextIndex = this.pageIndex + 1;
-                return path + nextIndex;
-            }
-            else {
-                document.querySelector('.view-more-button').style.display = 'none'
-            }
+            return path + "?page=" + (this.pageIndex + 1);
         },
         append: '.post',
-        button: '.view-more-button',
+        button: '.js-view-more-button',
         scrollThreshold: false,
-        status: '.page-load-status'
+        status: '.page-load-status',
+        checkLastPage: '#js-more-pages-indicator'
     });
 }
