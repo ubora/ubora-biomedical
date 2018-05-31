db32a38b0d2f86c3c3de2c472efc5855d7933a2c
kasparkallas <kaspar.kallas@agileworks.eu>
2018-05-31 14:13:21 +0300

    Unembed migration to fix functional tests on build server

--- src/Ubora.Web/Startup.cs
+++ src/Ubora.Web/Startup.cs
@@ -189,7 +189,6 @@ namespace Ubora.Web
                 var documentStore = serviceProvider.GetService<IDocumentStore>();
                 var domainMigrator = serviceProvider.GetService<DomainMigrator>();
 
-                documentStore.Schema.ApplyAllConfiguredChangesToDatabase(); // Creates all the tables Marten has been configured for. The initial reason I added this here is for the migrations to work on an empty database that run on the build server.
                 domainMigrator.MigrateDomain(ConnectionString);
                 documentStore.Schema.WritePatchByType("Patches");
 
