e5ce0d0d09f43b495b9e3cc29a1097cf0ddab92b
Kärolin Kivisikk <karolin.kivisikk@gmail.com>
2018-06-12 11:44:55 +0300

    'i have a need' proto

---
+++ src/Ubora.Web/_Features/Needs/NeedsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ubora.Web._Features.Needs
+{
+    [Route("[controller]/[action]")]
+    public class NeedsController : UboraController
+    {
+        // URL: /Needs/First
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        public IActionResult Second()
+        {
+            return View();
+        }
+
+        public IActionResult Third()
+        {
+            return View();
+        }
+
+        public IActionResult Fourth()
+        {
+            return View();
+        }
+    }
+}
