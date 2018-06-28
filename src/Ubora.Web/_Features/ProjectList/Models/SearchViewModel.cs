ec3725e696c667d481432faebc334fdc493fcdf1
Martin Betlem <martin.betlem@gmail.com>
2018-06-28 17:14:14 +0300

    Move areas to selectList.

--- src/Ubora.Web/_Features/ProjectList/Models/SearchViewModel.cs
+++ src/Ubora.Web/_Features/ProjectList/Models/SearchViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Ubora.Web._Features._Shared;
 
 namespace Ubora.Web._Features.ProjectList.Models
 {
@@ -10,69 +12,6 @@ namespace Ubora.Web._Features.ProjectList.Models
         public ByStatusFilteringMethod ByStatus { get; set; }
         public SortBy SortBy { get; set; }
         public ProjectListViewModel ProjectListViewModel { get; set; }
-        public List<string> AreaOfUsageTags { get; set; } =
-            new List<string> { "Cardiovascular surgery",
-                                "Colorectal surgery",
-                                "General surgery",
-                                "Neurosurgery",
-                                "Oncologic surgery",
-                                "Ophthalmic surgery",
-                                "Oral and maxillofacial surgery",
-                                "Orthopedic surgery",
-                                "Otolaryngology",
-                                "Pediatric surgery",
-                                "Plastic surgery",
-                                "Podiatric surgery",
-                                "Transplant surgery",
-                                "Trauma surgery",
-                                "Urology",
-                                "Vascular surgery",
-
-                                "Angiology / vascular medicine",
-                                "Cardiology",
-                                "Critical care medicine",
-                                "Endocrinology",
-                                "Gastroenterology",
-                                "Geriatrics",
-                                "Hematology",
-                                "Hepatology",
-                                "Infectious disease",
-                                "Nephrology",
-                                "Neurology",
-                                "Oncology",
-                                "Pediatrics",
-                                "Pneumology / chest medicine",
-                                "Rheumatology",
-                                "Sports medicine",
-                                "Traumatology and orthopedics",
-
-                                "Athology",
-                                "Cellular pathology",
-                                "Clinical chemistry",
-                                "Clinical immunology",
-                                "Clinical microbiology",
-                                "Clinical neurophysiology",
-                                "Diagnostic radiology",
-                                "Hematology",
-                                "Molecular biology",
-                                "Nuclear medicine",
-                                "Transfusion medicine",
-
-                                "Anesthesiology",
-                                "Dermatology",
-                                "Emergency medicine",
-                                "Family medicine",
-                                "Gerontology",
-                                "Medical genetics",
-                                "Neurology",
-                                "Obstetrics and gynecology",
-                                "Ophthalmology",
-                                "Pediatrics",
-                                "Pharmaceutical medicine",
-                                "Preventive medicine",
-                                "Psychiatry",
-                                "Public health",
-                                "Rehabilitation"
-                            };
+        public List<string> AreaOfUsageTags { get; set; } = SelectLists.Areas.Select(a => a.Text).ToList();
     }
 }
