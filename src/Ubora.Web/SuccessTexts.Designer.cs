472be8ff4bb2b8691d75bc38046dac44ec5ac6c4
kikkoi <kikkoi123@gmail.com>
2018-07-23 10:43:56 +0300

    Change value to File deleted.

--- src/Ubora.Web/SuccessTexts.Designer.cs
+++ src/Ubora.Web/SuccessTexts.Designer.cs
@@ -19,7 +19,7 @@ namespace Ubora.Web {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SuccessTexts {
@@ -394,7 +394,7 @@ namespace Ubora.Web {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to File hidden.
+        ///   Looks up a localized string similar to File deleted.
         /// </summary>
         public static string RepositoryFileHidden {
             get {
