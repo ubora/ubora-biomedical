f99f72c48e86db73083e62613501be1d6f3f1100
kasparkallas <kaspar.kallas@agileworks.eu>
2018-07-30 11:53:38 +0300

    WP6 starter controller for proto

---
+++ src/Ubora.Web/_Features/Projects/Workpackages/Wp6Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Ubora.Web._Features.Projects._Shared;
+
+namespace Ubora.Web._Features.Projects.Workpackages
+{
+    [ProjectRoute("wp6")]
+    public class Wp6Controller : ProjectController
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            ViewData["MenuOption"] = ProjectMenuOption.Workpackages;
+            ViewData["WorkpackageMenuOption"] = WorkpackageMenuOption.WP4StructuredInformationOnTheDevice;
+        }
+
+        [Route("proto")]
+        public IActionResult Proto()
+        {
+            return View();
+        }
+    }
+}
