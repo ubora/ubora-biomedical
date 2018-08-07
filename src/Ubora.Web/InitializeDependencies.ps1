d1ff362accd3dfc82e296e844620931ba50f0615
kikkoi <kikkoi123@gmail.com>
2018-08-07 10:57:22 +0300

    Add build converter in initialize dependencies script.

--- src/Ubora.Web/InitializeDependencies.ps1
+++ src/Ubora.Web/InitializeDependencies.ps1
@@ -11,3 +11,5 @@ if(!$isAzureEmulatorRunning) {
     docker rm -f ubora_azurite
     docker run -d -p 32500:10000 --name "ubora_azurite" mpahk/azurite
 }
+
+powershell ../Ubora.UniversalDocumentConverter/BuildAndRunConverter.ps1
