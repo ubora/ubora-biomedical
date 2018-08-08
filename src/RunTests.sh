c1c973d5a5e829d48da245bac6bd61b3a9812fcc
kikkoi <kikkoi123@gmail.com>
2018-08-08 13:33:38 +0300

    Fix RunTests.sh

--- src/RunTests.sh
+++ src/RunTests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Runs all tests and returns nonzero exit code, if any failed
-# using -framework netcoreapp2.1 -fxversion 2.1.1, which .NET Core SDK targeting 2.1.1
-(cd Ubora.Domain.Tests && dotnet xunit -teamcity -framework netcoreapp2.1 -fxversion 2.1.1 --configuration Release)
+(cd Ubora.Domain.Tests && dotnet test --verbosity normal --configuration Release)
 exit_code=$?
-(cd Ubora.Web.Tests && dotnet xunit -teamcity -framework netcoreapp2.1 -fxversion 2.1.1 --configuration Release)
+(cd Ubora.Web.Tests && dotnet test --verbosity normal --configuration Release)
 let exit_code=$?+$exit_code
 if [ $exit_code -ne 0 ]; then
     echo "Some tests failed!"
