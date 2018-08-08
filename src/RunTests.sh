61d0b25de04606be135ca0a3f20cd5b95d3722b8
kikkoi <kikkoi123@gmail.com>
2018-08-08 11:44:19 +0300

    Updgrade docker files and script.

--- src/RunTests.sh
+++ src/RunTests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Runs all tests and returns nonzero exit code, if any failed
-# using -framework netcoreapp2.0 -fxversion 2.0.6, which aspnetcore-build targeting 2.0.6
-(cd Ubora.Domain.Tests && dotnet xunit -teamcity -framework netcoreapp2.0 -fxversion 2.0.6 --configuration Release)
+# using -framework netcoreapp2.1 -fxversion 2.1.1, which .NET Core SDK targeting 2.1.1
+(cd Ubora.Domain.Tests && dotnet xunit -teamcity -framework netcoreapp2.1 -fxversion 2.1.1 --configuration Release)
 exit_code=$?
-(cd Ubora.Web.Tests && dotnet xunit -teamcity -framework netcoreapp2.0 -fxversion 2.0.6 --configuration Release)
+(cd Ubora.Web.Tests && dotnet xunit -teamcity -framework netcoreapp2.1 -fxversion 2.1.1 --configuration Release)
 let exit_code=$?+$exit_code
 if [ $exit_code -ne 0 ]; then
     echo "Some tests failed!"
