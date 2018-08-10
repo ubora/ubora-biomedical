5f834d23081d21e0b2c979367c092c39514f8fe5
kikkoi <kikkoi123@gmail.com>
2018-08-10 10:32:34 +0300

    add build with no incremental.

--- src/RunCIBuild.sh
+++ src/RunCIBuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-
 rm -f -r Ubora.Web/obj/Docker/publish
-dotnet restore ./Ubora.sln && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
+dotnet restore ./Ubora.sln && dotnet build ./Ubora.sln --no-incremental && bash RunTests.sh && dotnet publish ./Ubora.sln -c Release -o ./obj/Docker/publish
