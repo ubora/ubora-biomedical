00688dff7a1911ac5300cbcffd5589e35542c4d5
Kärolin Kivisikk <karolin.kivisikk@gmail.com>
2018-08-17 15:52:51 +0300

    Added university and country to member card

--- src/Ubora.Web/_Features/Users/UserList/Models/UserListItemViewModel.cs
+++ src/Ubora.Web/_Features/Users/UserList/Models/UserListItemViewModel.cs
@@ -7,6 +7,8 @@ namespace Ubora.Web._Features.Users.UserList.Models
         public Guid UserId { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string Country { get; set; }
+        public string University { get; set; }
         public string ProfilePictureLink { get; set; }
         public string Role { get; set; }
         public bool IsInvited { get; set; }
