aa2fd1fc84b9de2c52b827985a11d5c9b1babd3a
kasparkallas <kaspar.kallas@agileworks.eu>
2018-09-02 14:41:53 +0300

    Merge follow-up

---
+++ src/Ubora.Web/_Features/Projects/_Shared/ProjectQuillToolbarFormatOptions.cs
@@ -0,0 +1,18 @@
+﻿using Ubora.Web._Features._Shared;
+
+namespace Ubora.Web._Features.Projects._Shared
+{
+    public class ProjectQuillToolbarFormatOptions : QuillToolbarFormatOptions
+    {
+        public ProjectQuillToolbarFormatOptions()
+        {
+            TextDecorations = true;
+            TextSubAndSuperScript = true;
+            Blockquote = true;
+            Lists = true;
+            Hyperlinks = true;
+            Images = true;
+            Videos = true;
+        }
+    }
+}
